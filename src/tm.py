60e32ad4e8ea79f8b8b054a40557c40711aa3202
Alx2003 <alexanderqlay@gmail.com>
2024-05-09 23:29:58 -0400

    Move cpu data collection into tm.py

--- src/tm.py
+++ src/tm.py
@@ -1,13 +1,31 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, DataTable
+import psutil as psu
+import asyncio
+
+DATA_HEADINGS = [("CPU Utilization:",),]
+KEYS = ["cpu_util",]
 
 class TM(App):
    ENABLE_COMMAND_PALETTE = False
    BINDINGS = [("q", "quit", "Quit")]
+   header = Header()
+   table = DataTable(show_header=False, show_cursor=False)
+   footer = Footer()
 
    def compose(self) -> ComposeResult:
-      yield Header()
-      yield Footer()
+      yield self.header
+      yield self.table
+      yield self.footer
+
+   def on_mount(self) -> None:
+      self.table.add_columns(*DATA_HEADINGS[0])
+      for row in DATA_HEADINGS:
+         cpu_util = psu.cpu_percent(interval=1)
+         line = [f"{row[0]} {cpu_util}%",]
+         self.table.add_row(*line, key="cpu_util")
+
+   #Update polled value
 
    def action_quit(self) -> None:
       self.exit()
